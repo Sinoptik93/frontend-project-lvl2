71ec8bb153ac01876c6f4c54529f615d5a3a3beb
Sinner93 <chepukhin.sergey@gmail.com>
2020-05-26 22:53:06 +0500

    add test function for jest | step 3

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,5 +5,11 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.1', '-v, --version', 'output the version number')
   .option('-f, --format [type]', 'output format')
-  // eslint-disable-next-line no-undef
+  .arguments('<filepath1> <filepath2>')
+  .action((a, b) => {
+    const result = Number(a) + Number(b);
+    console.log(result);
+  })
   .parse(process.argv);
+
+console.log(process.argv);
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import fs from 'fs';
+import path from 'path';
+
+const gendiff = (a) => {
+  console.log('work!');
+  return a * 2;
+};
+
+export default gendiff;
