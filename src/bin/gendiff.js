177468ccc331490a59087b5c4acddb2bada47c3f
Sinner93 <chepukhin.sergey@gmail.com>
2020-05-28 22:00:26 +0500

    add main function in commander action | step 3

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,15 +1,11 @@
 #!/usr/bin/env node
 import program from 'commander';
+import gendiff from '../index.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.1', '-v, --version', 'output the version number')
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
-  .action((a, b) => {
-    const result = Number(a) + Number(b);
-    console.log(result);
-  })
+  .action((pathfile1, pathfile2) => gendiff(pathfile1, pathfile2))
   .parse(process.argv);
-
-console.log(process.argv);
