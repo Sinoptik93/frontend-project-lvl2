e31975d10573e2c16f76330e2755062e691e0ace
Sinner93 <chepukhin.sergey@gmail.com>
2020-05-30 18:59:46 +0500

    add ".ini" parser | step 6

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import fs from 'fs';
 import yaml from 'js-yaml';
+import ini from 'ini';
 import path from 'path';
 import process from 'process';
-import compareFiles from './parser.js';
+import parseKeys from './parser.js';
 
 const normolizePath = (filepath) => {
   if (filepath.slice(0, 2) === './' || filepath.slice(0, 3) === '../') {
@@ -17,22 +18,19 @@ const chooseParserBy = (filepath) => {
   let parse;
   const fileFormat = path.extname(filepath);
 
-  if (fileFormat === '.json') {
-    parse = JSON.parse;
-  } else if (fileFormat === '.yml') {
-    parse = yaml.safeLoad;
+  switch (fileFormat) {
+    case '.json':
+      parse = JSON.parse;
+      break;
+    case '.yml':
+      parse = yaml.safeLoad;
+      break;
+    case '.ini':
+      parse = ini.parse;
+      break;
+    default:
+      throw new Error(`Unknown file extention '${fileFormat}'.\nPlease, check file at directory: ${filepath}`);
   }
-  // switch (fileFormat) {
-  //   case fileFormat === ".json":
-  //     parse = JSON.parse;
-  //     break;
-  //   case fileFormat === ".yml":
-  //     parse = yaml.safeLoad;
-  //     break;
-  //   default:
-  //     console.log(`Filepath: ${filepath}\nFile format: ${fileFormat}\n`);
-  //     throw new Error('Unknown file extention!');
-  // }
   return parse;
 };
 
@@ -43,7 +41,7 @@ const gendiff = (filepath1, filepath2) => {
   const parsedFile1 = fileParser1(fs.readFileSync(normolizePath(filepath1), 'utf-8'));
   const parsedFile2 = fileParser2(fs.readFileSync(normolizePath(filepath2), 'utf-8'));
 
-  const result = compareFiles(parsedFile1, parsedFile2);
+  const result = parseKeys(parsedFile1, parsedFile2);
 
   console.log(result);
   return result;
