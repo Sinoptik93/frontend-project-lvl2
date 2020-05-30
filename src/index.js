4d9c085112d2d23866cdc9a3f6914808c14f5d99
Sinner93 <chepukhin.sergey@gmail.com>
2020-05-30 13:29:48 +0500

    add parser by extention

--- src/index.js
+++ src/index.js
@@ -1,64 +1,51 @@
 import fs from 'fs';
+import yaml from 'js-yaml';
 import path from 'path';
 import process from 'process';
+import compareFiles from './parser.js';
 
-const compareFunction = (file1, file2) => {
-  const compareResult = [];
-  const allKeys = [...new Set([...Object.keys(file1), ...Object.keys(file2)])];
-  // Lint error: https://eslint.org/docs/rules/no-prototype-builtins
-  const hasProperty = (file, key) => Object.prototype.hasOwnProperty.call(file, key);
+const normolizePath = (filepath) => {
+  if (filepath.slice(0, 2) === './' || filepath.slice(0, 3) === '../') {
+    const currentDirectory = process.cwd();
+    const absolutePath = path.resolve(currentDirectory, filepath);
+    return absolutePath;
+  }
+  return filepath;
+};
 
-  allKeys.forEach((key) => {
-    // Similar keys and ...
-    if (hasProperty(file1, key) && hasProperty(file2, key)) {
-      // similar values.
-      if (file1[key] === file2[key]) {
-        compareResult.push([' ', key, file1[key]]);
-      } else {
-        // different values.
-        let valueGreater;
-        let valueSmaller;
-        if (file1[key] > file2[key]) {
-          [valueGreater, valueSmaller] = [file1[key], file2[key]];
-          compareResult.push(['+', key, valueSmaller], ['-', key, valueGreater]);
-        } else {
-          [valueSmaller, valueGreater] = [file1[key], file2[key]];
-          compareResult.push(['-', key, valueSmaller], ['+', key, valueGreater]);
-        }
-      }
-    } else if (hasProperty(file1, key) && !hasProperty(file2, key)) {
-      // No key in file2.
-      compareResult.push(['-', key, file1[key]]);
-    } else {
-      // No key in file1.
-      compareResult.push(['+', key, file2[key]]);
-    }
-  });
+const chooseParserBy = (filepath) => {
+  let parse;
+  const fileFormat = path.extname(filepath);
 
-  let stringifiedResult = '';
-  // Make result string
-  compareResult.forEach((stringData) => {
-    const compareResultString = `${stringData[0]} ${stringData[1]}: ${stringData[2]}`;
-    stringifiedResult = `${stringifiedResult}\n${compareResultString}`;
-  });
-  console.log(`{${stringifiedResult}\n}`);
-  return `{${stringifiedResult}\n}`;
+  if (fileFormat === '.json') {
+    parse = JSON.parse;
+  } else if (fileFormat === '.yml') {
+    parse = yaml.safeLoad;
+  }
+  // switch (fileFormat) {
+  //   case fileFormat === ".json":
+  //     parse = JSON.parse;
+  //     break;
+  //   case fileFormat === ".yml":
+  //     parse = yaml.safeLoad;
+  //     break;
+  //   default:
+  //     console.log(`Filepath: ${filepath}\nFile format: ${fileFormat}\n`);
+  //     throw new Error('Unknown file extention!');
+  // }
+  return parse;
 };
 
 const gendiff = (filepath1, filepath2) => {
-  const normolizePath = (filepath) => {
-    if (filepath.slice(0, 2) === './' || filepath.slice(0, 3) === '../') {
-      const currentDirectory = process.cwd();
-      const absolutePath = path.resolve(currentDirectory, filepath);
-      return absolutePath;
-    }
-    return filepath;
-  };
+  const fileParser1 = chooseParserBy(filepath1);
+  const fileParser2 = chooseParserBy(filepath2);
 
-  const stringifiedFile1 = JSON.parse(fs.readFileSync(normolizePath(filepath1), 'utf-8'));
-  const stringifiedFile2 = JSON.parse(fs.readFileSync(normolizePath(filepath2), 'utf-8'));
+  const parsedFile1 = fileParser1(fs.readFileSync(normolizePath(filepath1), 'utf-8'));
+  const parsedFile2 = fileParser2(fs.readFileSync(normolizePath(filepath2), 'utf-8'));
 
-  const result = compareFunction(stringifiedFile1, stringifiedFile2);
+  const result = compareFiles(parsedFile1, parsedFile2);
+
+  console.log(result);
   return result;
 };
 
