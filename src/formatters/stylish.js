05c680e9505614d388223be38acc04467d443cd2
Sergey Chepukhin <s.chepukhin@iq-adv.ru>
2022-02-10 22:55:46 +0500

    refactor-1 | fix tree output

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
 /**
- * Stringify object by current indent
+ * Stringify object by current indent.
  * @param object{{}}
  * @param nodeDepth{number}
  * @return {string}
@@ -9,7 +9,9 @@ import _ from 'lodash';
 const stringifyObject = (object, nodeDepth) => {
   const innerIndent = _.repeat(' ', nodeDepth * 4 + 8);
   const closeIndent = _.repeat(' ', nodeDepth * 4 + 4);
+
   const keys = Object.keys(object);
+
   const mapper = (key) => {
     const value = object[key];
 
@@ -24,14 +26,15 @@ const stringifyObject = (object, nodeDepth) => {
 
 /**
  * Get styled string with depth indent.
- * @param node{{}}
+ * @param value{string|{}}
  * @param depth{number}
- * @param value{number}
  * @return {string}
  */
-const getString = (node, depth, value) => (_.isObject(value)
-  ? stringifyObject(value, depth)
-  : value);
+const getString = (value, depth) => (
+  _.isObject(value)
+    ? stringifyObject(value, depth)
+    : value
+);
 
 /**
  * Get tree styled output.
@@ -44,21 +47,29 @@ const getStylish = (data) => {
     const closeIndent = _.repeat(' ', depth * 4);
 
     const mapper = (node) => {
-      const stringOptions = {
-        removed: () => `${innerIndent}- ${node.key}: ${getString(node, depth, node.value)}`,
-        nested: () => `${innerIndent}  ${node.key}: ${iter(node.children, depth + 1)}`,
-        updated: () => `${innerIndent}- ${node.key}: ${getString(node, depth, node.value[0])}\n`
-          + `${innerIndent}+ ${node.key}: ${getString(node, depth, node.value[1])}`,
-        unchanged: () => `${innerIndent}  ${node.key}: ${getString(node, depth, node.value)}`,
-        added: () => `${innerIndent}+ ${node.key}: ${getString(node, depth, node.value)}`,
+      const {
+        key, value, status, children,
+      } = node;
+
+      const getStringWhich = {
+        unchanged: () => `${innerIndent}  ${key}: ${getString(value, depth)}`,
+        added: () => `${innerIndent}+ ${key}: ${getString(value, depth)}`,
+        removed: () => `${innerIndent}- ${key}: ${getString(value, depth)}`,
+        nested: () => `${innerIndent}  ${key}: ${iter(children, depth + 1)}`,
+        updated: () => {
+          const [beforeValue, afterValue] = value;
+          return `${innerIndent}- ${key}: ${getString(beforeValue, depth)}\n`
+          + `${innerIndent}+ ${key}: ${getString(afterValue, depth)}`;
+        },
       };
 
-      return stringOptions[node.status]();
+      return getStringWhich[status]();
     };
     const resultString = currentData.map((dataItem) => mapper(dataItem)).join('\n');
 
     return `{\n${resultString}\n${closeIndent}}`;
   };
+
   return iter(data, 0);
 };
 
